a0153d388cda8ce9fc40fdfda176e12a3f522b82
wawanwfs <wahyufajarsetiawan1999@gmail.com>
2025-03-31 05:36:07 +0700

    Simple CRUD Laravel 12

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,4 @@ use Illuminate\Support\Facades\Route;
 # route dengan mode resources
 Route::resource(name: '/products', controller: ProductController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index'])->name('dashboard');
