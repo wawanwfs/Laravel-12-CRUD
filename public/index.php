dab646d426a0b9b564eaa7ab7ca4312dbee423c9
wawanwfs <wahyufajarsetiawan1999@gmail.com>
2025-03-31 05:05:14 +0700

    Fix workflow

--- public/index.php
+++ public/index.php
@@ -1,47 +1,20 @@
 <?php
 
-// Path ke folder aplikasi Laravel
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
 define('LARAVEL_START', microtime(true));
 
-// Periksa apakah sedang berjalan di GitHub Pages
-$basePath = '/';
-if (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'github.io') !== false) {
-    // Ekstrak nama repository dari URL jika pada github.io
-    $uriParts = explode('/', $_SERVER['REQUEST_URI']);
-    if (count($uriParts) > 1) {
-        $basePath = '/' . $uriParts[1] . '/';
-    }
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
 
-// Register Composer autoloader
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    require __DIR__ . '/../vendor/autoload.php';
-} else {
-    die('Composer autoloader not found. Please run "composer install".');
-}
+// Register the Composer autoloader...
+require __DIR__ . '/../vendor/autoload.php';
 
-// Load environment variables
-if (file_exists(__DIR__ . '/../.env')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-    $dotenv->load();
-}
-
-// Set base path untuk GitHub Pages
-if (strpos($_SERVER['HTTP_HOST'] ?? '', 'github.io') !== false) {
-    $_SERVER['SCRIPT_NAME'] = $basePath . 'index.php';
-}
-
-// Bootstrap Laravel application
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Set public path
-$app->usePublicPath(__DIR__);
-
-// Run the application
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
